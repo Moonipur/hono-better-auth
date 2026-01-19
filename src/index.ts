8ce4dd717c126e9fcc234fdebe5c27a53bd85f3f
Moonipur <moo_sutthittha@hotmail.com>
2026-01-19 17:32:11 +0700

    Completed hono-better-auth backend + nodemailer

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 import { Hono } from "hono";
+import { auth } from "./lib/auth";
+import { cors } from "hono/cors";
+import { todos } from "./routes/todos.routes";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+app.use(
+  "/api/auth/*",
+  cors({
+    origin: "http://localhost:3000",
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["POST", "GET", "OPTIONS"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+    credentials: true,
+  })
+);
+
+app
+  .on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw))
+  .route("/api/todos", todos)
+  .get("/", (c) => {
+    return c.text("Hello Hono!");
+  });
 
 export default app;
